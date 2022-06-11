0ba4299a461e9ff3329ffb0b512ceeaed8071e9f
Marcus Griep <marcus@griep.us>
2022-06-11 12:07:43 -0400

    docs: update example crate

--- aliri_braid_examples/src/lib.rs
+++ aliri_braid_examples/src/lib.rs
@@ -2,14 +2,17 @@
 //! generate strongly-typed wrappers around string values.
 //!
 //! Three types of braids are demonstrated:
-//! * [Wrapper][wrapper] (and an example with [small-string optimization][sso_wrapper])
+//! * [Wrapper][wrapper]
+//!   * A wrapper around a string with [small-string optimizations][sso_wrapper]
+//!   * A wrapper around a string [backed by `Bytes`][bytes]
 //! * [Validated][validated]
 //! * [Normalized][normalized]
 //!
-//! In addition, the `minimal` module demonstrates the minimal string
+//! In addition, the [`minimal`] module demonstrates the minimal string
 //! implementation that can be wrapped inside a braid type.
 #![deny(unsafe_code)]
 
+pub mod bytes;
 pub mod minimal;
 pub mod normalized;
 pub mod ref_only;
